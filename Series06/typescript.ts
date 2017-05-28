d0f349fdd47a7329ff8b2bef1e54aaef737fe5ee
Peter <peter.meyer@stud.fh-flensburg.de>
2017-05-28 17:51:01 +0200

    commit

--- Series06/typescript.ts
+++ Series06/typescript.ts
@@ -2,6 +2,74 @@
  * Created by peter on 28.05.2017.
  */
 
+function count<T>(
+    list: [T]
+) :number {
+        return reduce(
+            x => y => x + 1,
+            0,
+            list
+        );
+}
+
+function sum (
+    numbers: [number]
+) :number {
+        return reduce(
+            x => y => x + y,
+            0,
+            numbers
+        )
+}
+
+function prod (
+    numbers: [number]
+) :number {
+        return reduce(
+            x => y => x * y,
+            1,
+            numbers
+        );
+}
+
+function and(
+    booleans: [boolean]
+) :boolean {
+        return reduce(
+            x => y => x && y,
+            true,
+            booleans
+        );
+}
+
+function map<T, R>(
+    f: (T) => (R),
+    list: [T]
+) :[R] {
+        return reduce(
+            x => y => {
+                x.push(f(y));
+                return x;
+            },
+            [],
+            list
+        );
+}
+
+function filter<T, R> (
+    p: (T) => (boolean),
+    list: [T]
+) :[R] {
+        return reduce(
+            x => y => {
+                if(p(y)) x.push(y);
+                return x;
+            },
+            [],
+            list
+        );
+}
+
 function reduce<T, R>(
     f: (R) => (T) => R,
     initial: R,
